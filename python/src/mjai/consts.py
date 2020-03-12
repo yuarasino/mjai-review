e4cd4ec9eb14057d7ab047f50c0f810c26027b4a
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2020-03-12 18:34:03 +0900

    :rocket: レビューモデルを更新

--- python/src/mjai/consts.py
+++ python/src/mjai/consts.py
@@ -1,1 +1,2 @@
 MJLOG_FETCH_URL = "https://tenhou.net/0/log/?{mjlog_id}"
+MJLOG_WATCH_URL = "https://tenhou.net/3/?log={mjlog_id}&tw={target_wind}"
--- python/src/review/models.py
+++ python/src/review/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
+from mjai import consts
+
 
 class Review(models.Model):
     class Meta:
@@ -11,6 +13,7 @@ class Review(models.Model):
         Reserved = 1, "予約中"
         Started = 2, "レビュー中"
         Ended = 3, "レビュー完了"
+        Error = 4, "エラー終了"
 
     mjlog_id = models.CharField("牌譜ID", max_length=63)
     target_wind = models.PositiveSmallIntegerField("視点", default=0)
@@ -21,3 +24,16 @@ class Review(models.Model):
     reserved_at = models.DateTimeField("予約時刻", default=timezone.now)
     started_at = models.DateTimeField("開始時刻", null=True, blank=True)
     ended_at = models.DateTimeField("終了時刻", null=True, blank=True)
+
+    @property
+    def mjlog_fetch_url(self) -> str:
+        """牌譜URL"""
+        return consts.MJLOG_FETCH_URL.format(mjlog_id=self.mjlog_id)
+
+    @property
+    def mjlog_watch_url(self) -> str:
+        """観戦URL"""
+        return consts.MJLOG_WATCH_URL.format(mjlog_id=self.mjlog_id, target_wind=self.target_wind)
+
+    def __str__(self):
+        return self.mjlog_watch_url
