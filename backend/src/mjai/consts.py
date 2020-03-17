b1986a34a250fdab6a4923f6b83ac7f32fa184ae
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2020-03-18 00:59:53 +0900

    :mahjong: コードを微修正

--- backend/src/mjai/consts.py
+++ backend/src/mjai/consts.py
@@ -1,3 +1,3 @@
-MJLOG_VIEW_URL_REGEX = r"^https://tenhou.net/3/"
-MJLOG_VIEW_URL_PLACEHOLDER = "https://tenhou.net/3/?log={mjlog_id}&tw={target_actor}"
+MJLOG_VIEW_URL_REGEX = r"^https://tenhou.net/\d/"
+MJLOG_VIEW_URL_PLACEHOLDER = "https://tenhou.net/0/?log={mjlog_id}&tw={target_actor}"
 MJLOG_FILE_URL_PLACEHOLDER = "https://tenhou.net/0/log/?{mjlog_id}"
